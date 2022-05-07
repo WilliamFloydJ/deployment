fd4ff7c1de2b01a79790aebfd6631ca4d788f920
WilliamFloydJ <floydfamily71@gmail.com>
2022-05-06 20:02:50 -0600

    Push

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../Public/index.html"));
 });
 
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Public/index.js"));
+});
+
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Public/main.css"));
+});
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
